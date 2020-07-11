beb99b7766d90065bfabf2c042cbe6b3e7c9f3a6
Brian Boylen <bbbrianboylen@gmail.com>
2020-07-11 16:07:00 -0500

    Added basic user signup / sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  root 'users#new'
 end
