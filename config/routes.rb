ad7ba92a61dc1ae71f403a3b750072f61268a2f6
Brian Boylen <bbbrianboylen@gmail.com>
2020-07-11 17:21:17 -0500

    Added event creation and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
+  resources :events, only: [:new, :create, :index, :show]
   resources :users
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
